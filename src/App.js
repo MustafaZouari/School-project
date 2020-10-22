b6d890e2396cb82272ba3f977a8511ef783eaf75
zouariM <spoward.dev.front@sossodigit.fr>
2020-10-22 19:59:05 +0100

    layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import NavBar from './components/navBar/NavBar';
+import './index.css';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import Home from './components/mainContent/Home';
+import HomePage from './components/pages/HomePage';
+import Courses from './components/pages/Courses';
+import Footer from './components/footer/Footer';
 
 function App() {
-    return (
-        <div>
-            App
-        </div>
-    )
+  return (
+    <div className='App'>
+      <Router>
+        <NavBar />
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/courses' component={Courses} />
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
