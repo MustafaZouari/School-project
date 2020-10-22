5736aee2aacd328a7cbaba7934734fadd13ab4d0
zouariM <spoward.dev.front@sossodigit.fr>
2020-10-22 21:17:31 +0100

    fix gitignore

--- dist/src.7ed060e2.js
+++ dist/src.7ed060e2.js
@@ -34129,7 +34129,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58012" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58477" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
